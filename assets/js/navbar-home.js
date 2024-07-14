5bd013d2207127d76a53aab1fb7dc5c13ed4abd8
muhammad ashari <boykid923@gmail.com>
2024-07-14 13:49:25 +0700

    js login

--- assets/js/navbar-home.js
+++ assets/js/navbar-home.js
@@ -1,25 +1,30 @@
 const navbarHome = document.getElementById("navbar-home");
-      const footer = document.getElementById("footer");
-      async function loadNavbarHome() {
-        const response = await fetch("./assets/component/navbar-home.html");
-        const data = await response.text();
-        navbarHome.innerHTML = data;
+const footer = document.getElementById("footer");
 
-        const hiddenMenu = document.getElementById("menu-hidden");
-        const menu = document.getElementById("menu");
-        const user = document.getElementById("user");
+async function loadNavbarHome() {
+  const response = await fetch("./assets/component/navbar-home.html");
+  const data = await response.text();
+  navbarHome.innerHTML = data;
+  
+  const btnLogin = document.getElementById("login");
+  const hiddenMenu = document.getElementById("menu-hidden");
+  const menu = document.getElementById("menu");
+  const user = document.getElementById("user");
 
-        hiddenMenu.onclick = () => {
-          if (menu.getAttribute("style")) {
-            menu.removeAttribute("style");
-          } else {
-            menu.setAttribute("style", "display: none");
-          }
-          if (user.getAttribute("style")) {
-            user.removeAttribute("style");
-          } else {
-            user.setAttribute("style", "display: none");
-          }
-        };
-      }
-      loadNavbarHome();
+  hiddenMenu.onclick = () => {
+    if (menu.getAttribute("style")) {
+      menu.removeAttribute("style");
+    } else {
+      menu.setAttribute("style", "display: none");
+    }
+    if (user.getAttribute("style")) {
+      user.removeAttribute("style");
+    } else {
+      user.setAttribute("style", "display: none");
+    }
+  };
+  btnLogin.onclick=()=>{
+    window.location="/signin.html"
+  }
+}
+loadNavbarHome();
